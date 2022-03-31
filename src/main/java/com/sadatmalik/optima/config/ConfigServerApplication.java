596a0b2e3f19f12c8332789ccb66d3da930bc92a
sadat.malik <sadat.malik@me.com>
2022-03-31 13:29:54 +0100

    ConfigServer configuration and properties

--- src/main/java/com/sadatmalik/optima/config/ConfigServerApplication.java
+++ src/main/java/com/sadatmalik/optima/config/ConfigServerApplication.java
@@ -2,8 +2,17 @@ package com.sadatmalik.optima.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Spring Cloud service bootstrap class that we can use to launch the service.
+ *
+ * The @EnableConfigServer class annotation enables the service as a Spring Cloud Config service.
+ *
+ * @author sadatmalik
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
